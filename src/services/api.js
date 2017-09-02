63ebfc1075c867b2606d08cc961f0ad94f3ae2a2
Erika Sankari <8eikochan@gmail.com>
2017-09-02 15:06:19 +0300

    Send rooms to php api.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
 import moment from "moment";
+import config from '../config';
 
 export const fetchReservations = async () => {
     const start = moment("2017-09-04T08:00");
     const end = moment(start).add(20, "hours");
 
-    const response = await fetch(`http://koti.tamk.fi/~c4vkinnu/ykampus/query.php?start=${start.toISOString()}&end=${end.toISOString()}`);
+    const response = await fetch(`${config.apiUrl}
+    ?start=${start.toISOString()}
+    &end=${end.toISOString()}
+    &rooms=${config.rooms.join(",")}`);
+
     return await response.json();
 };
