467e98213d5650e3a6cfcd55bffb77660cd2ad4f
Erika Sankari <8eikochan@gmail.com>
2017-09-02 15:01:05 +0300

    Create config.js.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiUrl: "http://koti.tamk.fi/~c4vkinnu/ykampus/query.php",
+
+    rooms: [
+        { code: "C1-22", name: "Backstage", reservations: [] },
+        { code: "C1-23", name: "Paja", reservations: [] },
+        { code: "C1-25", name: "Stage", reservations: [] },
+        { code: "C1-18", name: "Neuvottelutila", reservations: [] }
+    ]
+}
