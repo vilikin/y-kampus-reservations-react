137d3798c1d402b7e23eb1abb8b2dcf27dfc6a1c
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-09-02 14:47:36 +0300

    Fetch correct reservations.

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,9 @@
-export const fetchReservations = async (start, end) => {
-    const response = await fetch(`http://koti.tamk.fi/~c4vkinnu/ykampus/query.php?start=${start}&end=${end}`);
+import moment from "moment";
+
+export const fetchReservations = async () => {
+    const start = moment("2017-09-04T08:00");
+    const end = moment(start).add(20, "hours");
+
+    const response = await fetch(`http://koti.tamk.fi/~c4vkinnu/ykampus/query.php?start=${start.toISOString()}&end=${end.toISOString()}`);
     return await response.json();
 };
